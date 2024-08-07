d5d8c46701aa3ca7b17a827810b81a7d164cce32
jhspuk <j.hugh.sp@gmail.com>
2024-08-07 10:43:20 +0200

    brought in vex tooling

---
+++ vex/deps.sh
@@ -0,0 +1,26 @@
+# JAVA JDK 8
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk -y
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+
+# Install SBT - https://www.scala-sbt.org/
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+sudo apt-get install sbt
+
+# Verilator (for sim only, really needs 3.9+, in general apt-get will give you 3.8)
+sudo apt-get install git make autoconf g++ flex bison
+git clone http://git.veripool.org/git/verilator   # Only first time
+unsetenv VERILATOR_ROOT  # For csh; ignore error if on bash
+unset VERILATOR_ROOT  # For bash
+cd verilator
+git pull        # Make sure we're up-to-date
+git checkout v4.216
+autoconf        # Create ./configure script
+./configure
+make
+sudo make install
