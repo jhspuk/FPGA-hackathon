48df65d13f97ee8b5bdd7a41b6f3961f47c170c0
jhspuk <j.hugh.sp@gmail.com>
2024-08-07 16:10:02 +0200

    TA basic complete untested

--- tests/2/TA.v
+++ tests/2/TA.v
@@ -9,39 +9,87 @@ module ta(
 	input wire type_feedback, 
 	input wire clause_result,
 
+	input wire rand_clk,
 	input wire rand_in,
 
+	output wire ready;
 	output wire done,
 	output reg rand_out,
-	output reg ta_result,
+	output reg ta_result
 );
 
 
 	logic [2:0] internal_weight;
 
-	enum int unsigned {INIT, INFERENCE, TRAIN, FEEDBACK, OUT} state;
+	enum int unsigned [2:0] {INFERENCE, TRAIN, FEEDBACK, OUT} state;
+	enum int unsigned [1:0] {REW, PEN, IGN} train_state;
 
-	always @(posedge clk) begin
+	assign ready = (state == INFERENCE);
+	assign done = (state == OUT);
+
+	always @(posedge clk or posedge rst) begin
 		if (rst) begin
-			done <= 1'b0;
 			rand_out <= 1'b0;
 			ta_result <= 1'b0;
 			internal_weight <= 3'b000;
+			state = INFERENCE;
 		end else if(enable && !training_sel) begin
 			case (state)
-				INIT: begin
-
-				end
 				INFERENCE: begin
-
-				end
-				OUT: begin
-
+					ta_result <= literal_in || (internal_weight > 3'b010);
+					state <= OUT;
 				end
 		end else if(enable && training_sel) begin
-
+			case (state)
+				INFERENCE: begin
+					ta_result <= literal_in || (internal_weight > 3'b010);
+					state <= TRAIN;
+				end
+				TRAIN: begin
+					if (!type_feedback) begin
+						if (!(clause_result ^ (internal_weight > 3'b010)) begin
+							train_state <= REW;
+						end else if ((!clause_result) && (internal_weight > 3'b010)) begin
+							train_state <= PEN;
+						end else begin
+							train_state <= literal_in == 1 ? PEN : REW; 
+						end
+					end else begin
+						if (!(internal_weight > 3'b010) && clause_result && literal_in) begin
+							train_state <= PEN;
+						end else begin
+							train_state <= IGN;
+						end
+					end
+					state <= FEEDBACK;
+				end
+				FEEDBACK: begin
+					if (!type_feedback) begin
+						if (train_state == REW && (internal_weight < 3'b100)) begin
+							internal_weight <= internal_weight + (1 & rand_out);
+						end else if (train_state == PEN && (internal_weight > 3'b000)) begin
+							internal_weight <= internal_weight - (1 & rand_out);
+						end
+					end else begin
+						if (train_state == REW && (internal_weight < 3'b100)) begin
+							internal_weight <= internal_weight + 1;
+						end else if (train_state == PEN && (internal_weight > 3'b000)) begin
+							internal_weight <= internal_weight - 1;
+						end
+	
+					end
+				end
+		end else if(!enable) begin
+			state = INFERENCE;
 		end
 	end
 
 
-end
+	always @(posedge rand_clk or posedge rst) begin
+		if(rst) begin
+			rand_out <= 1'b0;
+		end
+	end
+
+
+endmodule
